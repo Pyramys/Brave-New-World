be53a73f9e1d014ee1caf2bd1ff69c4436b36235
Pyramys <Vycerys@gmail.com>
2018-12-20 16:56:51 -0700

    Start of Time work
    Most of hours and minutes mechanically are done. missing UI handling of it. Started period change too.

---
+++ A Brave New World/Assets/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public Text txt_day;
+    public Text txt_minute;
+    public Text txt_hour;
+    public Text txt_period;
+
+    public Time time;
+
+    private void Awake()
+    {
+        txt_day.text = "01";
+        txt_period.text = "Landing";
+    }
+
+    private void Start()
+    {
+        // Change where this actually starts.        
+        time.StartTime();
+    }
+
+    void Update()
+    {
+        UpdateTimeUI();
+    }
+
+    private void UpdateTimeUI ()
+    {
+        txt_minute.text = time.Int_minute.ToString();
+        txt_hour.text = time.Int_hour.ToString();
+    }
+}
