d5f2bb9e7c28b456cc07b1d69ad03e45191d49c0
Pyramys <Vycerys@gmail.com>
2018-12-22 00:04:01 -0700

    Started Basic Movement
    WASD movement is in the game. Need to work on rotation and jumping.

---
+++ A Brave New World/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+    [SerializeField] private float flt_moveSpeed =100;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }    
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(horizontal, 0, vertical);
+
+        characterController.SimpleMove(movement * Time.deltaTime * flt_moveSpeed);
+    }
+}
